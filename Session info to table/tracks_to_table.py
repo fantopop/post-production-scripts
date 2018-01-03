1f25b6b640d333e0391bcd4662cb9863a85433b1
Ilya Putilin <fantopop@gmail.com>
2018-01-03 10:09:37 +0300

    Rename Session info to table folder

--- Session info to table/tracks_to_table.py
+++ Session info to table/tracks_to_table.py
@@ -123,7 +123,7 @@ def outname(filename, ext='csv'):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Converts '.txt' file from Pro Tools 'Export Session Info as Text' command to '.csv' file")
+        description="Converts '.txt' file from Pro Tools 'Export Session Info as Text' command to '.csv' or '.html' file")
     parser.add_argument(
         'txt', metavar='textfile', type=argparse.FileType(mode='r'),
         help='session info text file from Pro Tools')
